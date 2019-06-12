8a7d8b50984bff7afdc9b5b80f8a6aa090b42bdd
zj3310012 <01654383@yto.net.cn>
2019-06-12 09:37:24 +0800

    update

---
+++ springcloud_integration/zipkin-server/src/main/java/com/zj/zipkinServer/ZipkinServerApplication.java
@@ -0,0 +1,18 @@
+package com.zj.zipkinServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import zipkin.server.EnableZipkinServer;
+
+@EnableDiscoveryClient
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+
+}
