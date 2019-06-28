bb7cc18d2c34d105d47a22d86075f18f6407111f
zj3310012 <01654383@yto.net.cn>
2019-06-28 11:36:49 +0800

    update

--- springcloud_integration/data-exchange/src/main/java/com/zj/dataExchange/config/SwaggerConfig2.java
+++ springcloud_integration/data-exchange/src/main/java/com/zj/dataExchange/config/SwaggerConfig2.java
@@ -23,7 +23,7 @@ public class SwaggerConfig2 {
 	@Bean
 	public Docket createRestApi() {
 		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-				.apis(RequestHandlerSelectors.basePackage("com.yt.dataExchange.web.controller")).paths(PathSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.zj.dataExchange.web.controller")).paths(PathSelectors.any())
 				.build();
 	}
 
